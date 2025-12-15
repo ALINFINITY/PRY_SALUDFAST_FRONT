712be5525cdae3d5f9e2a16a48a882e8efa13b81
Wladyes <wladyes@gmail.com>
2025-12-15 14:11:12 -0500

    feat: implementar módulo de registro de usuarios (paciente/médico)

--- src/modules/Routes_module.tsx
+++ src/modules/Routes_module.tsx
@@ -2,16 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import { HomeModule } from "./Home_module";
 import { NotFoundModule } from "./NotFound_module";
 import { PatientsModule } from "./Paciente_module";
+import { RegisterModule } from "./Register_module"; 
 
 export const RoutesModule = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomeModule />} />
+        <Route path="/" element={<HomeModule />} />        
         <Route path="*" element={<NotFoundModule />} />
-        {/*En este archivo deben ir registrando las rutas hacia sus modulos */}
-        <Route path="/pacientes" element={<PatientsModule/>}/>
+        <Route path="/pacientes" element={<PatientsModule />} />
+        <Route path="/register" element={<RegisterModule />} />
+
+        {/* En este archivo se registran las rutas de los módulos */}
       </Routes>
     </>
   );
 };
+
